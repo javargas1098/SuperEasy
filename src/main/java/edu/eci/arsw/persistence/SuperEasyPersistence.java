d5e04f1de23385ee70f2c8c1b9e50c0a29bafc22
Sebastian <sebastian.goenaga@hotmail.com>
2019-03-21 00:02:39 -0500

    fixing repository without succes

--- src/main/java/edu/eci/arsw/persistence/SuperEasyPersistence.java
+++ src/main/java/edu/eci/arsw/persistence/SuperEasyPersistence.java
@@ -1,7 +1,10 @@
 package edu.eci.arsw.persistence;
 
+import org.springframework.stereotype.Service;
+
 import edu.eci.arsw.model.User;
 
+@Service
 public interface SuperEasyPersistence {
 	
 	public void createUser (User user);

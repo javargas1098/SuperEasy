70e050417fc280166fbb6ad78771636862f09821
Sebastian <sebastian.goenaga@hotmail.com>
2019-04-08 22:07:25 -0500

    remove unusable classes

--- src/main/java/edu/eci/arsw/security/SpringSecurityConfig.java
+++ src/main/java/edu/eci/arsw/security/SpringSecurityConfig.java
@@ -1,61 +1,61 @@
-package edu.eci.arsw.security;
-
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-//@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Value("${spring.datasource.url}")
-	private String dbUrl;
-	@Value("${spring.datasource.username}")
-	private String dbUsername;
-	@Value("${spring.datasource.password}")
-	private String dbPassword;
-
+//package edu.eci.arsw.security;
+//
+//import java.sql.SQLException;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//
+////@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//	@Value("${spring.datasource.url}")
+//	private String dbUrl;
+//	@Value("${spring.datasource.username}")
+//	private String dbUsername;
+//	@Value("${spring.datasource.password}")
+//	private String dbPassword;
+//
+////	@Autowired
+//	public DataSource dataSource;
+//
+////	@Bean
+//	public DataSource dataSource() throws SQLException {
+//		if (dbUrl == null || dbUrl.isEmpty()) {
+//			return new HikariDataSource();
+//		} else {
+//			HikariConfig config = new HikariConfig();
+//			config.setJdbcUrl(dbUrl);
+//			config.setUsername(dbUsername);
+//			config.setPassword(dbPassword);
+//			return new HikariDataSource(config);
+//		}
+//	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.httpBasic().and().authorizeRequests().anyRequest().authenticated();
+//	}
+//
 //	@Autowired
-	public DataSource dataSource;
-
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.jdbcAuthentication().dataSource(dataSource)
+//				.usersByUsernameQuery("select email, password, 1 as enabled  from users where email=?");
+//	}
+//
 //	@Bean
-	public DataSource dataSource() throws SQLException {
-		if (dbUrl == null || dbUrl.isEmpty()) {
-			return new HikariDataSource();
-		} else {
-			HikariConfig config = new HikariConfig();
-			config.setJdbcUrl(dbUrl);
-			config.setUsername(dbUsername);
-			config.setPassword(dbPassword);
-			return new HikariDataSource(config);
-		}
-	}
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and().authorizeRequests().anyRequest().authenticated();
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery("select email, password, 1 as enabled  from users where email=?");
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-}
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//}
